111c616bec10382d1712a33cd0fd48cdb4d77a91
Nguyen Do <khoanguyen.do@outlook.com>
2025-12-24 10:28:39 +0700

    feat: aggregate stops

--- cmd/transit-watcher/main.go
+++ cmd/transit-watcher/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 	"os"
@@ -12,6 +13,11 @@ import (
 	"github.com/catouberos/transit-watcher/internal/aggregator"
 	"github.com/catouberos/transit-watcher/providers/gobus"
 	"github.com/catouberos/transit-watcher/providers/multigo"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
+	"go.opentelemetry.io/otel/log/global"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/log"
 )
 
 func main() {
@@ -21,6 +27,14 @@ func main() {
 	routeService := apiv1connect.NewRouteServiceClient(http.DefaultClient, "http://localhost:5001")
 	variantService := apiv1connect.NewVariantServiceClient(http.DefaultClient, "http://localhost:5001")
 	geolocationService := apiv1connect.NewGeolocationServiceClient(http.DefaultClient, "http://localhost:5001")
+	stopService := apiv1connect.NewStopServiceClient(http.DefaultClient, "http://localhost:5001")
+
+	// Set up OpenTelemetry.
+	otelShutdown, err := setupOTelSDK(ctx)
+	// Handle shutdown properly so nothing leaks.
+	defer func() {
+		err = errors.Join(err, otelShutdown(context.Background()))
+	}()
 
 	client := &http.Client{
 		Timeout: 2 * time.Minute,
@@ -30,6 +44,7 @@ func main() {
 		routeService,
 		variantService,
 		geolocationService,
+		stopService,
 		gobus.NewClient(client),
 		multigo.NewClient(client),
 	)
@@ -37,4 +52,62 @@ func main() {
 
 	<-ctx.Done()
 	slog.Info("attempt to gracefully shutdown")
+}
+
+// setupOTelSDK bootstraps the OpenTelemetry pipeline.
+// If it does not return an error, make sure to call shutdown for proper cleanup.
+func setupOTelSDK(ctx context.Context) (func(context.Context) error, error) {
+	var shutdownFuncs []func(context.Context) error
+	var err error
+
+	// shutdown calls cleanup functions registered via shutdownFuncs.
+	// The errors from the calls are joined.
+	// Each registered cleanup will be invoked once.
+	shutdown := func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	// Set up propagator.
+	prop := newPropagator()
+	otel.SetTextMapPropagator(prop)
+
+	// Set up logger provider.
+	loggerProvider, err := newLoggerProvider()
+	if err != nil {
+		handleErr(err)
+		return shutdown, err
+	}
+	shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
+	global.SetLoggerProvider(loggerProvider)
+
+	return shutdown, err
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
+
+func newLoggerProvider() (*log.LoggerProvider, error) {
+	logExporter, err := stdoutlog.New()
+	if err != nil {
+		return nil, err
+	}
+
+	loggerProvider := log.NewLoggerProvider(
+		log.WithProcessor(log.NewBatchProcessor(logExporter)),
+	)
+	return loggerProvider, nil
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,19 @@ require (
 	buf.build/gen/go/catou/transit-radar/protocolbuffers/go v1.36.10-20251017072010-ae2a9f9d5b9c.1
 	connectrpc.com/connect v1.19.1
 	github.com/cenkalti/backoff/v5 v5.0.3
+	go.opentelemetry.io/otel v1.38.0
+	go.opentelemetry.io/otel/exporters/stdout/stdoutlog v0.14.0
+	go.opentelemetry.io/otel/log v0.14.0
+	go.opentelemetry.io/otel/sdk/log v0.14.0
 )
 
-require google.golang.org/protobuf v1.36.10 // indirect
+require (
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.38.0 // indirect
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
+)
